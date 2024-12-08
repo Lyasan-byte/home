75c8fdc1fff551acbdc73220530f6be9ee642563
Lyasan Minlebaeva <lajsanminlebaeva@mail.ru>
2024-12-08 09:14:36 +0300

    stringBuilder + override toString

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        StringBuilder str = new StringBuilder("hi!");
+        str.append(" the day is hard").append(" and awfull");
+        System.out.println(str.toString());
+        System.out.printf("%d this is my %.3f string \n ", 4, 130.6798);
+
+        Person pasal = new Person("pasal", -30439);
+        System.out.println(pasal);
 
 
     }
